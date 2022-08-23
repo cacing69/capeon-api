8c7d77a9ab6e817921f378d2ad23298ea60c2b26
cacing69 <ibnuul@gmail.com>
2022-08-23 23:46:09 +0700

    revalidate

--- src/utils/filters/error.filter.ts
+++ src/utils/filters/error.filter.ts
@@ -11,6 +11,7 @@ export class ErrorFilter extends BaseExceptionFilter {
   catch(error: any, host: ArgumentsHost) {
     const changeCode = (code) => {
       const defineCode = {
+        400: '40000',
         401: '40100',
         404: '40400',
         500: '50000',
@@ -31,11 +32,15 @@ export class ErrorFilter extends BaseExceptionFilter {
 
     const getCode = error?.response?.statusCode || error?.response?.code || 500;
     const code = changeCode(getCode);
-    const message = error?.response?.message || 'something went wrong';
+    const message =
+      (error?.response?.message as string)?.toLowerCase() ||
+      'something went wrong';
     const extra =
       error?.response?.extra || [error.stack?.split('at')[0]?.trim()] || null;
     const meta = null;
     const data = null;
+
+    message;
 
     return response.status(status).send({ code, message, meta, data, extra });
 
